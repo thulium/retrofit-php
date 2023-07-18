339519ef1dfcbf9eebfc84a1abc091949223d1b4
Piotr Olaszewski <piotr.olaszewski@thulium.pl>
2023-07-18 13:03:36 +0200

    Apply rule nullable_type_declaration

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -52,6 +52,7 @@ return (new PhpCsFixer\Config())
         'no_unneeded_control_parentheses' => true,
         'no_unneeded_curly_braces' => true,
         'no_useless_else' => true,
+        'nullable_type_declaration' => true,
         'nullable_type_declaration_for_default_null_value' => true,
         'ordered_class_elements' => [
             'order' => [
