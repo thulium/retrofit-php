41b895cd2d31e18c0a00f09870ca0bbcb7dbe521
Krzysztof Piorkowski <krzysztof.piorkowski@thulium.pl>
2024-03-11 13:10:33 +0100

    Added test cases for POST calls

--- tests/Core/FunctionalRetrofitTest.php
+++ tests/Core/FunctionalRetrofitTest.php
@@ -13,6 +13,9 @@ use Retrofit\Client\Guzzle7\Guzzle7HttpClient;
 use Retrofit\Converter\SymfonySerializer\SymfonySerializerConverterFactory;
 use Retrofit\Core\Retrofit;
 use Retrofit\Tests\Fixtures\Api\FullyValidApi;
+use Retrofit\Tests\Fixtures\Model\UserRequest;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\JsonSerializableNormalizer;
 use Symfony\Component\Serializer\Serializer;
 
 class FunctionalRetrofitTest extends TestCase
@@ -39,7 +42,7 @@ class FunctionalRetrofitTest extends TestCase
     }
 
     #[Test]
-    public function shouldGenerateProperResponse(): void
+    public function shouldGenerateProperResponseOnGetCall(): void
     {
         // given
         $this->mockWebServer->setResponseOfPath('/info/sample-user', new Response('foo bar content'));
@@ -50,15 +53,88 @@ class FunctionalRetrofitTest extends TestCase
         $result = $call->execute();
 
         // then
+        $this->assertEquals(200, $result->code());
         $this->assertEquals('foo bar content', $result->body());
+    }
+
+    #[Test]
+    public function shouldGenerateProperResponseOnPostCall(): void
+    {
+        // given
+        $this->mockWebServer->setResponseOfPath('/users/sample-user', new Response('foo bar content'));
+
+        $userRequest = (new UserRequest())
+            ->setId(1)
+            ->setLogin('sample-user');
+
+        $call = $this->fullyValidApi->createUser('sample-user', $userRequest);
+
+        // when
+        $result = $call->execute();
+
+        // then
+        $this->assertEquals(200, $result->code());
+        $this->assertEquals('foo bar content', $result->body());
+    }
+
+    #[Test]
+    public function shouldHandleRequestOnPostCall(): void
+    {
+        // given
+        $this->mockWebServer->setResponseOfPath('/users/sample-user', new Response('foo bar content'));
+
+        $userRequest = (new UserRequest())
+            ->setId(1)
+            ->setLogin('sample-user');
+
+        $call = $this->fullyValidApi->createUser('sample-user', $userRequest);
+
+        // when
+        $call->execute();
+
+        // then
+        $input = $this->mockWebServer->getLastRequest()->getInput();
+        $this->assertJson($input);
+        $this->assertEquals('{"id":1,"login":"sample-user"}', $input);
+    }
+
+    #[Test]
+    public function shouldProcessUrlBeforePath(): void
+    {
+        // given
+        $this->mockWebServer->setResponseOfPath('/users/jon', new Response('foo bar content'));
+
+        $call = $this->fullyValidApi->pathIsBeforeUrl('jon', 'https://thulium.pl/users/{login}');
+
+        // when
+        $call->execute();
+
+        // then
+        $this->assertSame('/users/jon', $call->request()->getUri()->getPath());
+    }
+
+    #[Test]
+    public function shouldAddQueryStringToUrlAttribute(): void
+    {
+        // given
+        $call = $this->fullyValidApi->urlWithQuery('new', 'https://thulium.pl/users');
+
+        // when
+        $call->execute();
+
+        // then
+        $this->assertSame('group=new', $call->request()->getUri()->getQuery());
     }
 
     private function createFullyValidApiMock(): FullyValidApi
     {
+        $normalizers = [new JsonSerializableNormalizer()];
+        $encoders = [new JsonEncoder()];
+        $serializer = new Serializer($normalizers, $encoders);
         $retrofit = Retrofit::Builder()
             ->baseUrl($this->mockWebServer->getServerRoot())
             ->client(new Guzzle7HttpClient(new Client()))
-            ->addConverterFactory(new SymfonySerializerConverterFactory(new Serializer()))
+            ->addConverterFactory(new SymfonySerializerConverterFactory($serializer))
             ->build();
 
         /** @var FullyValidApi $service */
